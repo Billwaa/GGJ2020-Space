d9df7009af1f7114bcd9f2b5be424ef0267403ae
Billy Hau <billwaahau@gmail.com>
2020-02-02 11:52:55 -0500

    Music threshold tune... audio do not destory... cheat

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -5,16 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
+
+    musicControl mc;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mc = GameObject.FindObjectOfType<musicControl>();
+        mc.filterThreshhold = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mc.filterThreshhold < 5)
+            mc.filterThreshhold += 0.1f * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+            SceneManager.LoadScene(0);
+        else if (Input.GetKey(KeyCode.Alpha2))
+            SceneManager.LoadScene(1);
+        else if (Input.GetKey(KeyCode.Alpha3))
+            SceneManager.LoadScene(2);
+
     }
 
     private void OnCollisionEnter(Collision collision)
